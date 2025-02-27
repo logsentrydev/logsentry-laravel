a579dc99c2c4490517f66ce9976e112e0c8c42fb
Audun Larsen <larsen@xqus.com>
2025-02-28 00:35:57 +0100

    🔥 Remove unused methods

--- src/LogLevels.php
+++ src/LogLevels.php
@@ -20,35 +20,4 @@ class LogLevels
 
     public const DEBUG = 7;
 
-    public static function logLevelText($level)
-    {
-        $logLevelText = [
-            self::EMERGENCY => 'emergency',
-            self::ALERT => 'alert',
-            self::CRITICAL => 'critical',
-            self::ERROR => 'error',
-            self::WARNING => 'warning',
-            self::NOTICE => 'notice',
-            self::INFO => 'info',
-            self::DEBUG => 'debug',
-        ];
-
-        return $logLevelText[$level];
-    }
-
-    public static function logLevelColor($level)
-    {
-        $logLevelColor = [
-            self::EMERGENCY => 'rose',
-            self::ALERT => 'red',
-            self::CRITICAL => 'orange',
-            self::ERROR => 'amber',
-            self::WARNING => 'yellow',
-            self::NOTICE => 'sky',
-            self::INFO => 'teal',
-            self::DEBUG => 'zinc',
-        ];
-
-        return $logLevelColor[$level];
-    }
 }
